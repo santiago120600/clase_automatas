0dad2b6ec700d5db37ffb7cb4ec9a1b0a978076c
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-03-10 14:59:43 -0600

    arreglado el incremento

--- automata_02_2.py
+++ automata_02_2.py
@@ -2,53 +2,75 @@
 
 def automata(palabra):
     estado = 0
-    resultado = True
-    suma = 0
     enteros = ['0','1','2','3','4','5','6','7','8','9']
     if palabra:
+        print('***************************************\ncaracter | representa\n***************************************')
+        # estado 0
+        if palabra[0] in enteros:
+            estado=1
+        elif palabra[0] == '*':
+            estado=3
+        elif palabra[0] == '+':
+            estado=2
+        # estado 0
         for letra in palabra:
-            if(estado==0):
+            if(estado==1):
                 if letra == '+':
+                    print('+ | suma')
+                    print('---------------------------------------')
                     estado = 2
                 elif letra in enteros:
+                    print(letra,'| numero')
+                    print('---------------------------------------')
                     estado = 1
                 elif letra == '*':
-                    estado = 3
-                else:
-                    return False
-            elif(estado==1):
-                if letra == '+':
-                    estado = 2
-                elif letra in enteros:
-                    estado = 1
-                elif letra == '*':
+                    print('* | producto')
+                    print('---------------------------------------')
                     estado = 3
                 else:
                     return False
             elif(estado==2):
                 if letra == '+':
+                    print('+ | suma')
+                    print('---------------------------------------')
                     estado = 4
                 elif letra in enteros:
+                    print(letra,'| numero')
+                    print('---------------------------------------')
                     estado = 1
                 elif letra == '*':
+                    print('* | producto')
+                    print('---------------------------------------')
                     estado = 3
                 else:
                     return False
             elif(estado==3):
                 if letra == '+':
+                    print('+ | suma')
+                    print('---------------------------------------')
                     estado = 2
                 elif letra in enteros:
+                    print(letra,'| numero')
+                    print('---------------------------------------')
                     estado = 1
                 elif letra == '*':
+                    print('* | producto')
+                    print('---------------------------------------')
                     estado = 3
                 else:
                     return False
             elif(estado==4):
                 if letra == '+':
+                    print('++ | incremento')
+                    print('---------------------------------------')
                     estado = 2
                 elif letra in enteros:
+                    print(letra,'| numero')
+                    print('---------------------------------------')
                     estado = 1
                 elif letra == '*':
+                    print('* | producto')
+                    print('---------------------------------------')
                     estado = 3
                 else:
                     return False
@@ -58,4 +80,4 @@ def automata(palabra):
         return False
 
 
-automata('0123')
+automata('++++0012*')
